9303babaeb64de31f3b55497ace657d3e9d664a2
杨再涛 <1772081518@qq.com>
2026-04-10 11:57:32 +0800

    调整

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import AppBar from "@/components/AppBar";
 import { useThemeContext } from "@/hook/ThemeProvider";
 import "@/design/App.css";
 
-function App() {
+export default function() {
   const context = useThemeContext();
-  return <main data-theme={context.val} className="rounded-xl overflow-y-auto" style={{ height: '100vh' }}>
+  return <main data-theme={context.val} className="rounded-sm overflow-y-auto" style={{ height: '100vh' }}>
     <AppBar />
     <div className="p-1">
+      <button className="btn btn-sm btn-primary">btn</button>
       {/* <span>{greetMsg}123</span>
       <input type="text" className="input input-sm input-primary px-1 mx-1" />
       <button className="btn btn-sm btn-primary" onClick={greet}>Button</button> */}
     </div>
   </main>
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./view/App";
-import ThemeProvider from "./hook/ThemeProvider";
+import App from "@/App";
+import ThemeProvider from "@/hook/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
